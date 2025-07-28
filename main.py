e4d24fd2921be2e14b2e5fcee96175e3aceaa2db
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-07-28 21:12:35 +0530

    super

--- main.py
+++ main.py
@@ -30,6 +30,8 @@
                         not = inverts the condition (not False, not True)"""
 from traceback import print_tb
 
+# from example import square
+
 """--------------------or-----------------------"""
 # temp = 20
 # is_raining = True
@@ -884,3 +886,59 @@ def area(radius):
 # fish.sleep()
 # fish.eat()
 # fish.flee()
+
+# -----------------------------super-----------------------------------------
+#
+# """super() = Function used in a child class to call methods from a parent class (superclass)
+#              Allows you to extend the functionality of a child class"""
+#
+# class Shape:
+#     def __init__(self, color, is_filled):
+#         self.color = color
+#         self.is_filled = is_filled
+#
+#     def describe(self):
+#         print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}.")
+#
+#
+# class Circle(Shape):
+#     def __init__(self, color, is_filled, radius):
+#         super().__init__(color, is_filled)
+#         self.radius = radius
+#
+#     def describe(self):   # method overriding
+#         super().describe()
+#         print(f"It is a circle with area of {3.14 * self.radius * self.radius}cm^2.")
+#
+# class Square(Shape):
+#     def __init__(self, color, is_filled, width):
+#         super().__init__(color, is_filled)
+#         self.width = width
+#
+#     def describe(self):   # method overriding
+#         super().describe()
+#         print(f"It is a Square with area of {self.width * self.width}cm^2.")
+#
+# class Rectangle(Shape):
+#     def __init__(self, color, is_filled, width, height):
+#         super().__init__(color, is_filled)
+#         self.width = width
+#         self.height = height
+#
+#     def describe(self):   # method overriding
+#         super().describe()
+#         print(f"It is a Rectangle with area of {self.width * self.height}cm^2.")
+#
+# circle = Circle(color = "red", is_filled = True, radius = 5)
+# square = Square(color = "red", is_filled = True, width = 5)
+# rectangle = Rectangle(color = "red", is_filled = True, width = 5, height = 3)
+#
+# # print(circle.color)
+# # print(circle.is_filled)
+# # print(f"{square.color} cm")
+#
+# circle.describe()
+# print()
+# square.describe()
+# print()
+# rectangle.describe()

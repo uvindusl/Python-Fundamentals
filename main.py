f44bbf17b0bc1bf5e63046758f04814a150da390
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-07-29 18:21:39 +0530

    add

--- main.py
+++ main.py
@@ -1131,3 +1131,58 @@ def area(radius):
 
 # student1 = Student('Bro', 22)
 # student2 = Student('Code', 33)
+
+# ----------------------------- @property -----------------------------------------
+
+"""@property = Decorator used to defined a method as a property (it can be accessed like an attribute)
+               Benefit: Add additional logic when read, write or delete attributes
+               Gives you getter, setter and deleter method"""
+
+
+# class Rectangle:
+#     def __init__(self, width, height):
+#         self._width = width
+#         self._height = height
+#
+#     @property
+#     def width(self):
+#         return f"{self._width:.1f}"
+#
+#     @property
+#     def height(self):
+#         return f"{self._height:.1f}"
+#
+#     @width.setter
+#     def width(self, new_width):
+#         if new_width > 0:
+#             self._width = new_width
+#         else:
+#             print("width must be greater than 0")
+#
+#     @height.setter
+#     def height(self, new_height):
+#         if new_height > 0:
+#             self._height = new_height
+#         else:
+#             print("height must be greater than 0")
+#
+#     @width.deleter
+#     def width(self):
+#         del self._width
+#         print("Width has been deleted")
+#
+#     @height.deleter
+#     def height(self):
+#         del self._height
+#         print("Height has been deleted")
+#
+# rectangle = Rectangle(3, 4)
+#
+# rectangle.width = 5
+# rectangle.height = 6
+#
+# del rectangle.width
+# del rectangle.height
+
+# print(rectangle.width)
+# print(rectangle.height)
